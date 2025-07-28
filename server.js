c7a12f08d3100e443026afe79e60386ee99e4d8b
NdikumNdifor <sebastienndifor@gmail.com>
2025-07-28 14:36:26 -0700

    initiate views using ejs and express layouts as view engine

--- server.js
+++ server.js
@@ -1,6 +1,56 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts')
 const connectDB = require('./database/connections');
 require('dotenv').config();
+
+
+/* ***********************
+ * Middleware for express sessions
+ * ************************/
+
+
+// Express Messages Middleware
+
+
+// will allow the cookie parser to be
+// implemented throughout the project.
+
+
+// JWT mildware
+
+
+
+/* ***********************
+ * View Engine and Template
+ *************************/
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layouts', './layouts/layout')
+
+/* ***********************
+ * Routes
+ *************************/
+
+
+/****************** 
+*  Gets and renders the index page at the root of the folder
+*********************/
+
+
+
+
+
+// File Not Found Route - must be last route in list
+
+
+// JWT mildware
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+
 
 // Start server
 const host = process.env.HOST;
